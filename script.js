2edd0783729181928fd33af2c6459bd2cbd95542
Dmitry <dmitrichenko.dima@yandex.ru>
2026-03-24 00:38:12 +0300

    added searchFilter

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//1.*****Ссылки на элементы
+const productContainer = document.getElementById("product-container");
+//связываем переменную с счетчиком
+let cartCount = document.getElementById("cart-count");
+const searchInput = document.getElementById("search-input");
+
+//2***** Данные (Data)
 //Создаю товары
 const products = [
   { name: "Биокамин", img: "image/Fireplace.jpg", price: 1265, id: 1 },
@@ -9,31 +16,53 @@ const products = [
     id: 3,
   },
 ];
-const productContainer = document.getElementById("product-container");
-//связываем переменную с счетчиком
-let cartCount = document.getElementById("cart-count");
 //Создаю корзину
 let shopping = [];
 
+//3.*****
 //.forEach()- перебор всех элементов массива и выполнение функции для каждого
-products.forEach((item) => {
-  const cardHtml = `<div class = "card">
+function renderProducts(productsArray) {
+  // 1. Сначала полностью очищаем контейнер, чтобы товары не дублировались
+  productContainer.innerHTML = "";
+  // 2. Используем именно productsArray (тот, что пришел в скобках)
+
+  productsArray.forEach((item) => {
+    const cardHtml = `<div class = "card">
   <h3>${item.name}</h3>
  <img src="${item.img}" alt="${item.name}" ">
   <p>Цена: ${item.price} руб.</p>
   <button class="buy-btn" data-id="${item.id}">Купить</button>
   </div>`;
-  //.insertAdjacentHTML() — это метод, который позволяет «вклеить» кусок HTML-кода (строку) в определенное место на странице.
-  productContainer.insertAdjacentHTML("beforeend", cardHtml);
-});
+    //.insertAdjacentHTML() — это метод, который позволяет «вклеить» кусок HTML-кода (строку) в определенное место на странице.
+    productContainer.insertAdjacentHTML("beforeend", cardHtml);
+  });
+}
+renderProducts(products);
 
 productContainer.addEventListener("click", function (ev) {
   //«Посмотри на тот элемент, по которому реально кликнули, и проверь — есть ли у него класс 'buy-btn'?»
   if (ev.target.classList.contains("buy-btn")) {
-    const id = ev.target.dataset.id;
+    // 1. Сразу добавляем класс анимации и меняем текст
+    // ev.target.classList.add("added");
+    // ev.target.textContent = "Добавлено!";
+
+    // // 2. Ставим таймер на возврат текста и класса через 1 сек
+    // setTimeout(() => {
+    //     ev.target.classList.remove("added");
+    //     ev.target.textContent = "Купить";
+    // }, 1000);
+
+    ev.target.classList.add("added");
+    ev.target.textContent = "Добавлено!";
+    setTimeout(() => {
+      ev.target.classList.remove("added");
+      ev.target.textContent = "Купить";
+    }, 700);
+
+    const id = parseInt(ev.target.dataset.id);
 
     //*Код написанный с помощью замены цикла for методомм .find() */
-    const foundProduct = products.find((item) => item.id == id);
+    const foundProduct = products.find((item) => item.id === id);
     shopping.push(foundProduct);
     console.log("В корзине сейчас: ", shopping);
 
@@ -42,7 +71,7 @@ productContainer.addEventListener("click", function (ev) {
 
     // let foundProduct;
     // for (let i = 0; i < products.length; i++) {
-    //   if (products[i].id == id) {
+    //   if (products[i].id === id) {
     //     foundProduct = products[i];
     //     break;
     //   }
@@ -50,4 +79,13 @@ productContainer.addEventListener("click", function (ev) {
     // shopping.push(foundProduct);
     // console.log("В корзине сейчас: ", shopping);
   }
+});
+
+searchInput.addEventListener("input", (ev) => {
+  const searchTextLow = ev.target.value.toLowerCase();
+  const filterProducts = products.filter((item) => {
+    return item.name.toLowerCase().includes(searchTextLow);
+  });
+
+  renderProducts(filterProducts);
 });
