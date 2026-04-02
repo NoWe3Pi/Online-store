b571c31a04049fdd9282c65bd27790220e684fb7
Dmitry <dmitrichenko.dima@yandex.ru>
2026-04-02 22:04:55 +0300

    Change Logo

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-//1.*****Ссылки на элементы
+//**1 Ссылки на элементы*/
 const productContainer = document.getElementById("product-container");
 //связываем переменную с счетчиком
 let cartCount = document.getElementById("cart-count");
 //ссылка на связь в поле ввода input, для сброса не найденных товаров
 const searchInput = document.getElementById("search-input");
-
 //привязка ссылки корзины
 const shoppingСart = document.getElementById("cart-box");
 
-//2***** Данные (Data)
-//Создаю товары
+// Ниже функция превратит 14500 в "14 500" с красивым пробелом
+function formattedPrice(formatted) {
+  return formatted.toLocaleString("ru-RU");
+}
+
+//**2 Данные (Data)*/
 const products = [
   { name: "Биокамин", img: "image/Fireplace.jpg", price: 1265, id: 1 },
   { name: "Дренажный насос", img: "image/optimize.jpg", price: 2500, id: 2 },
@@ -20,15 +23,15 @@ const products = [
     id: 3,
   },
 ];
-//Создаю корзину
+//Корзина
 let shopping = [];
 
-//3.*****
-//.forEach()- перебор всех элементов массива и выполнение функции для каждого
+//**3. Основная логика */
 function renderProducts(productsArray) {
   // 1. Сначала полностью очищаем контейнер, чтобы товары не дублировались
   productContainer.innerHTML = "";
 
+  //Логика если поисковая строка без совпадений с товарами
   if (productsArray.length === 0) {
     productContainer.innerHTML = `<div class='no-products'>
             <p>К сожалению, товары не найдены 🔍</p>
@@ -46,12 +49,17 @@ function renderProducts(productsArray) {
     return; // Производительность (return): Функция сразу прекращает работу, не пытаясь перебирать массив (хоть он и пустой).
   }
 
-  // 2. Используем именно productsArray (тот, что пришел в скобках)
+  //**4. ОТРИСОВКА КАРТОЧЕК ТОВАРА */
+
+  //.forEach()- перебор всех элементов массива и выполнение функции для каждого
   productsArray.forEach((item) => {
+    //форматирую item.price в productsArray
+    const formattedProducts = formattedPrice(item.price);
+
     const cardHtml = `<div class = "card">
   <h3>${item.name}</h3>
  <img src="${item.img}" alt="${item.name}" ">
-  <p>Цена: ${item.price} руб.</p>
+  <p>Цена: ${formattedProducts} руб.</p>
   <button class="buy-btn" data-id="${item.id}">Купить</button>
   </div>`;
     //.insertAdjacentHTML() — это метод, который позволяет «вклеить» кусок HTML-кода (строку) в определенное место на странице.
@@ -71,21 +79,14 @@ function renderProducts(productsArray) {
 }
 renderProducts(products);
 
+//**5. ИЗМЕНЕНИЕ КНОПКИ КУПИТЬ, ПРИ КЛИКЕ*/
 productContainer.addEventListener("click", (ev) => {
   //«Посмотри на тот элемент, по которому реально кликнули, и проверь — есть ли у него класс 'buy-btn'?»
   if (ev.target.classList.contains("buy-btn")) {
-    // 1. Сразу добавляем класс анимации и меняем текст
-    // ev.target.classList.add("added");
-    // ev.target.textContent = "Добавлено!";
-
-    // // 2. Ставим таймер на возврат текста и класса через 1 сек
-    // setTimeout(() => {
-    //     ev.target.classList.remove("added");
-    //     ev.target.textContent = "Купить";
-    // }, 1000);
-
+    // Добавляем класс анимации .added и меняем текст
     ev.target.classList.add("added");
     ev.target.textContent = "Добавлено!";
+    //Ставим таймер на возврат текста и класса через 1 сек
     setTimeout(() => {
       ev.target.classList.remove("added");
       ev.target.textContent = "Купить";
@@ -93,22 +94,21 @@ productContainer.addEventListener("click", (ev) => {
 
     const id = parseInt(ev.target.dataset.id);
 
-    //*Код написанный с помощью замены цикла for методомм .find() */
     const foundProduct = products.find((item) => item.id === id);
     const productInCart = shopping.find((item) => item.id === id);
 
     if (productInCart) {
       // В JavaScript объекты передаются по ссылке. Когда ты нашел товар через .find() и положил его в переменную productInCart, эта переменная — не копия товара, а «пульт управления» тем самым объектом, который уже лежит в массиве shopping.
       productInCart.count++;
-      //Счетчик после кнопки купить от 2 нажатий начинает отображаться
+      //Счетчик кнопки 'Купить' после 2-х кликов (2,3,4... и тд)
       ev.target.textContent = `Добавлено (${productInCart.count})`;
-    } else {
+    } //выполсяется блок else при первом клике, т.к товара в корзине нет и вернется undefined, что для JavaScript означает (false) в условие if()
+    else {
       foundProduct.count = 1; // ВОТ ЗДЕСЬ ПОЯВИЛОСЬ .count!
       shopping.push(foundProduct);
       console.log("В корзине сейчас: ", shopping);
     }
-    // «Пройдись по списку и сократи (reduce)чисел до одного числа(суммы)»
-    // Как это происходит в памяти (пошагово):
+
     // Представь, что в shopping лежат: Камин (count: 2) и Светильник (count: 3).
     // Шаг 0: Бухгалтер берет калькулятор. На экране: 0 (наше начальное значение).
     // Шаг 1: Берет Камин. Смотрит в его count (там 2). Прибавляет к нулю: 0 + 2 = 2. Теперь в копилке (acc) лежит 2.
@@ -125,13 +125,18 @@ productContainer.addEventListener("click", (ev) => {
 searchInput.addEventListener("input", (ev) => {
   //.trim() убирает лишние пробелы в начале и в конце строки. Так поиск не будет пытаться искать товары, состоящие из одних пробелов.
   const searchTextLow = ev.target.value.toLowerCase().trim();
+  //Cоздаю новый массив filterProducts(метод filter так работает), содержащие подстроку из searchTextLow
   const filterProducts = products.filter((item) => {
     return item.name.toLowerCase().includes(searchTextLow);
   });
 
   renderProducts(filterProducts);
+
+  /*!!!! КЛАССНАЯ ИНФА МОГУ ЗАБЫТЬ пустой .includes("") - true - как будто совпадения есть со всеми карточками, его особенность, так как пустота «есть» в любом тексте
+  "Биокамин".includes("Био") — true (совпадение есть)."Биокамин".includes("") — true (пустота «есть» в любом тексте).*/
 });
 
+//**6.ОБРАБОТЧИК КЛИКА И ПЕРЕХОД ВНУТРЬ КОРЗИНЫ*/
 shoppingСart.addEventListener("click", (ev) => {
   productContainer.innerHTML = "";
   if (shopping.length === 0) {
@@ -143,10 +148,13 @@ shoppingСart.addEventListener("click", (ev) => {
 
   document.querySelector("h1").innerHTML = "Корзина";
   shopping.forEach((item) => {
+    //форматирую item.price в shopping
+    const formattedShopping = formattedPrice(item.price);
+
     const cardShop = `<div class = "card">
    <h3>${item.name}</h3>
   <img src="${item.img}" alt="${item.name}" ">
-   <p>Цена: ${item.price} руб.</p>
+   <p>Цена: ${formattedShopping} руб.</p>
    <p>Количество: ${item.count} шт.</p>
    </div>`;
     productContainer.insertAdjacentHTML("beforeend", cardShop);
@@ -156,8 +164,9 @@ shoppingСart.addEventListener("click", (ev) => {
   }, 0);
 
   // 3. ОТРИСОВКА ИТОГА: Рисуем финальный блок (ВНЕ цикла, один раз)
+  const formattedtotalPriceShopping = formattedPrice(totalPriceShopping);
   const totalPriceShoppingHtml = `<div class="total-price-shopping">
-  <h3>Итог по оплате: ${totalPriceShopping}</h3>
+  <h3>Итог по оплате: ${formattedtotalPriceShopping}</h3>
   </div>`;
   productContainer.insertAdjacentHTML("beforeend", totalPriceShoppingHtml);
 });
